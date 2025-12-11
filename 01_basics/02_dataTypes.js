fd5e5f8f8dd755cac1173b7529c8c485d350d84f
Vaishnavi-Lele <vaishnavilele14@gmail.com>
2025-12-11 09:05:17 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,25 @@
 "use strict"; //treat all js code as newer version
 
 //alert(3+3) //we are using node.js not browser
+
+// js documentation MDN , original js documentation is tc39.es (ecma script)
+
+let name = "Vaishnavi"
+let age = 18
+let isLoggedIn = false
+let state = null;
+
+/* primitive datatypes
+ number => 2 to power 53
+ bigint
+ strint => ""
+boolean => true/false
+null => stand alone value
+undefined => value does not assign
+symbol =>unique
+*/ 
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
