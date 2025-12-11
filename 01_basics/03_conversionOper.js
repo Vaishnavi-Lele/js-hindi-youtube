d0abb67df41ba4fdacb0024a158a6ec5332e3acc
Vaishnavi-Lele <vaishnavilele14@gmail.com>
2025-12-11 11:47:50 +0000

    update done

--- 01_basics/03_conversionOper.js
+++ 01_basics/03_conversionOper.js
@@ -11,14 +11,36 @@ console.log(valueInNumber); // value comes as NaN that means not a number becaus
 //"33abc" => NaN
 //true => 1; false => 0
 
-let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 //1 => true; 0 => false
 // "" => false
 //"hitesh" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//****************Operations**************** //
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+ 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 ="hello "
+let str2 = "vaishnavi"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+ 
