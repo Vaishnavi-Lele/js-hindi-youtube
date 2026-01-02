e0469b7ad1e0383d9ed9b7829c43ad9a0c3501d3
Vaishnavi-Lele <vaishnavilele14@gmail.com>
2026-01-02 11:52:34 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+function sayMyName() {
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+//sayMyName()
+
+// function AddNo(number1, number2) {
+
+//     console.log(number1 + number2);
+// }
+
+function AddNo(number1, number2) {
+    // let res = number1 + number2
+    // return res
+  return number1 + number2
+}
+
+const res = AddNo(3, 4)
+console.log("Result", res);
