97203d1b99913499decbebec9a36ec03f86a4361
Vaishnavi-Lele <vaishnavilele14@gmail.com>
2026-01-19 08:35:08 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+//if
+// const isUserloggedIn = true
+// let temprature =41 
+// if(temprature > 50){
+// console.log("temp is less than 50");
+// }else{
+//     console.log("temp is greater than 50");
+    
+// }
+// console.log("not exicuted");
+ 
+//else if ladder
+// let balance =1000
+// if (balance < 500){
+//     console.log("less than 500");
+    
+// }
+// else if (balance < 750){
+//     console.log("less than 750");
+    
+// }
+// else {
+//     console.log("greater than 900");
+    
+// }
+
+const isUserloggedIn = true
+const debitCard = true
+if(isUserloggedIn && debitCard){
+    console.log("Allow to buy");
+    
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,25 @@
+const userEmail = []
+// if(userEmail){
+//     console.log("got user email");
+    
+// }else {
+//     console.log(("don't get email"));   
+// }
+
+
+//falsy values
+// false , 0, -0, BigInt, 0n,"", null, undefined, NaN
+
+//truthy values
+//"0", 'false', " ",[], {}, function(){}
+
+// if(userEmail.length === 0){
+//     // console.log("Array empty");
+    
+// }
+
+const emptObj = {}
+if(Object.keys(emptObj).length === 0) {
+console.log("OBJ EMPTY");
+
+}
