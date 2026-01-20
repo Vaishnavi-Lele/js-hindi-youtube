939e6f58cb989816aac21752b83f17e6ae94f1c3
Vaishnavi-Lele <vaishnavilele14@gmail.com>
2026-01-20 15:52:05 +0000

    iterations

---
+++ 05_iterations/three.js
@@ -0,0 +1,39 @@
+//for of loop
+
+//["", "", ""]
+//[{},{},{}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for(const num of arr){
+//     console.log(num);
+    
+// }
+
+// const greetings = "hello world!"
+// for (const greet of greetings){
+//     console.log(`Each char is ${greet}`);
+//     }
+
+//maps
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States Of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+//console.log(map);
+
+// for(const [key, value] of map){
+//     console.log(key, ':-', value);
+    
+// }
+
+const myObj = {
+    'game1' : 'NFS',
+    'game2' : 'spiderman'
+}
+// for(const [key, value] of myObj){
+//     console.log(key, ':-', value);
+    
+// }
